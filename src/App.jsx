0ca0945bbd3a2ebedaf8de055eadd251d3f4b8e1
afisman <afisman@gmail.com>
2023-06-30 12:52:20 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+
+import './App.css'
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
