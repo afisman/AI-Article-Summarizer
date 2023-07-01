43e8c5f019f1a26354e13d1873677c5a41ca1a1f
afisman <afisman@gmail.com>
2023-07-01 20:06:29 +0200

    worked on Demo component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                 <span className='orange_gradient'>OpenAI GPT-4</span>
             </h1>
             <h2 className='desc'>
-                Simp`lify your reading with Summize, an open-source article summarizer
+                Simplify your reading with Summize, an open-source article summarizer
                 that transforms lengthy articles into clear and concise summaries.
             </h2>
         </header>
